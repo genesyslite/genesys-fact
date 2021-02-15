fd1407b069ef0ca11c55d8ba56a98c5dac9ca163
gensysliteperu <62081383+gensysliteperu@users.noreply.github.com>
2021-02-14 19:39:39 -0500

    Fixed add middleware auth:api

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,6 +1,9 @@
 <?php
+
 use \Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->group(function () {
-    Route::post('documents', [\GenesysLite\GenesysFact\Http\Controllers\Api\V1\DocumentController::class, 'store']);
+Route::middleware(['auth:api'])->group(function () {
+    Route::prefix('api/v1')->group(function () {
+        Route::post('documents', [\GenesysLite\GenesysFact\Http\Controllers\Api\V1\DocumentController::class, 'store']);
+    });
 });
